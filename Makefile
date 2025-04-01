28a8b6ee6000ca24aaa370f151407e9cf267969e
Paul Blair <blairpaul8@yahoo.com>
2025-04-01 12:02:38 -0400

    Adding myshell to github

--- Makefile
+++ Makefile
@@ -1,10 +1,11 @@
 GXX = gcc
+CFLAGS = -fsanitize=address -g3
 TARGET = myshell
 
 all: $(TARGET)
 
 $(TARGET): myshell.c
-	$(GXX) myshell.c -o $(TARGET)
+	$(GXX) $(CFLAGS) myshell.c -o $(TARGET)
 
 clean:
 	rm -f $(TARGET)
--- myshell.c
+++ myshell.c
@@ -5,103 +5,138 @@
 #include <sys/wait.h>
 #include <string.h>
 
-#define PARSE_DELIM " \t\r\n\a"
+#define DELIMITERS " \t\r\n\a"
+#define GREEN "\033[32m"
+#define RESET "\033[0m"
+#define BLUE "\033[34m"
+
+int execute(char** args) {
+  //char* command = "ls";
+  pid_t pid;
+  pid_t wpid;
+  int status;
+
+  pid = fork(); 
+
+  //printf("ppid = %d\n", pid);  
+  //printf("pid = %d\n", pid);  
+
+  if (pid ==  0) {
+    //printf("pid = %d\n", pid);  
+    execvp(args[0], args);
+  }
+  else if (pid < 0) {
+    perror("Fork failed.\n");
+    return 0;
+  }
+  else {
+    wpid = wait(&status);
+    //printf("parent waiting pid = %d\n", pid);  
+    //printf("  WIFEXITED:    %d\n", WIFEXITED(status));
+  }
+
+  return 1;
+}
 
 char** parse_input(char *input) {
-	int buffsize = 64;
-	int position = 0;
-	char **tokens = malloc(buffsize * sizeof(char*));
-	char* token;
+  int num_tokens = 10;
+  char** tokens;
+  char* token;
+  int token_count = 0;
 
-	if (!tokens) {
-		fprintf(stderr, "parse_input allocation error\n");
-		exit(EXIT_FAILURE);
-	}
-
-	token = strtok(input, PARSE_DELIM);
-	while (token != NULL) {
-		tokens[position] = token;
-		position++;
-
-
-		if (position >= buffsize) {
-			buffsize += buffsize;
-			tokens = realloc(tokens, buffsize * sizeof(char*));
-			if (!tokens) {
-				fprintf(stderr, "reallocation error");
-				exit(EXIT_FAILURE);
-			}
-		}
-
-		token = strtok(NULL, PARSE_DELIM);
-	}
-	
-	tokens[position] = NULL;
-
-	return tokens;
-}
-
-int execute(const char** args)  {
+  tokens = (char**)malloc(num_tokens * sizeof(char*));
+  if (tokens == NULL) {
+    perror("Failed to allocate memory for tokens");
+    return EXIT_SUCCESS;
+  }
   
-  return -1;
+  token = strtok(input, DELIMITERS);
+
+  while (token != NULL) {
+    if (token_count >= num_tokens) {
+      num_tokens *= 2;
+      tokens = (char**)realloc(tokens, num_tokens * sizeof(char*));
+    }
+    tokens[token_count] = token;
+    token_count++;
+    token = strtok(NULL, DELIMITERS);
+  }
+
+  tokens[token_count] = NULL;
+
+  /*printf("Tokens:\n");
+  printf("   ");
+  for (int i = 0; i < token_count; i++) {
+    printf("%s, ", tokens[i]);
+  }
+  printf("\n");
+ */ 
+  return tokens;
 }
 
-char* get_input(char* input) {
+ssize_t get_input(char** input, size_t *buff, int delim) {
+  *buff = 64;
+  if (*input == NULL) {
+    *input = (char*)malloc(*buff * sizeof(char));
+  }
   int c;
   int i = 0;
-  int len = 0;
+  ssize_t len = 0;
 
   while ((c = getchar()) != '\n') {
-    input[i] = c;
+    if (i >= *buff) {
+      *buff += *buff;
+      *input = (char*)realloc(*input, *buff * sizeof(char*));
+
+    }
+    (*input)[i] = c;
     i++;
     len++;
   }
   
-  input[len] = '\0';
-  
-	return input;
-}
-
-void print_input(char* input) {
-  int i = 0;
-  while (input[i] != '\0') {
-    printf("%c", input[i]);
-    i++;
-  }
-  printf("\n");
+  (*input)[len] = '\0';
+   
+	return len;
 }
 
 void sh_loop(void) {
 	//Used for the prompt
 	char cwd[1024];
 	char *user = getlogin();
-  int buff = 1000;
-	char* input;
-  input = (char*)malloc(buff * sizeof(char));
+	char* input = NULL;
+  size_t buff = 0;
+  ssize_t linelen;
 	char** args;
 	int status;
 
 	getcwd(cwd, sizeof(cwd));
+  char* home = getenv("HOME");
+
 	system("clear");
 	do {
-		printf("%s: %s\n >> ",user, cwd);
+    if (home != NULL && (strncmp(home, cwd, strlen(home)) == 0)) {
+		  printf(GREEN "%s:" BLUE "~%s\n"" >> " RESET,user, cwd + strlen(home));
 
-		input = get_input(input);
-    print_input(input);
-		//args = parse_input(input);
-		//status = execute(args);
+    }
+    else {
+		  printf(GREEN "%s:" BLUE "%s\n"" >> " RESET,user, cwd);
+    }
 
-		free(input);
-		free(args);
+		linelen = get_input(&input, &buff, '\n');
+    //printf("%s\n", input);
+		args = parse_input(input);
+		status = execute(args);
+    
 
 	} while (status);
+	free(input);
+	free(args);
 
 }
 
 int main() {
 
 	sh_loop();
-
 
 	return 0;
 }
